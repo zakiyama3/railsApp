219d359d9cb5311912e0854593d430605de7a5ee
Yamazaki Nobuaki <33247309+zakiyama3@users.noreply.github.com>
2020-08-18 22:32:26 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  resources :microposts
-  resources :users
-  # root 'users#index'
-  root 'application#hello'
+  root 'static_pages#home'  
+  get  'static_pages/home'
+  get  'static_pages/help'
+  get  'static_pages/about'
+  get  'static_pages/contact'
 end
